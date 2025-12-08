d9a5405e038beabc10f3952c27af5aff9e62ebcc
briodan <41130293+briodan@users.noreply.github.com>
2025-12-08 09:46:50 +0000

    modified:   custom_components/mybusstop/config_flow.py

--- custom_components/mybusstop/config_flow.py
+++ custom_components/mybusstop/config_flow.py
@@ -80,10 +80,6 @@ class MyBusStopConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 class MyBusStopOptionsFlow(config_entries.OptionsFlow):
     """Handle options flow for MyBusStop."""
 
-    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        """Initialize options flow."""
-        self.config_entry = config_entry
-
     async def async_step_init(self, user_input: Optional[Dict[str, Any]] = None) -> FlowResult:
         """Manage the options."""
         if user_input is not None:
